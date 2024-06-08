383cd4f530b9df741040252dd53a4319e275c80d
JessiePinkMann <egor.anoshin.03@mail.ru>
2024-06-08 21:37:48 +0300

    Initial Commit

---
+++ Frontend_Scrabble/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  Frontend_Scrabble
+//
+//  Created by Egor Anoshin on 08.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Frontend_Scrabble/Frontend_ScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Frontend_ScrabbleApp.swift
+//  Frontend_Scrabble
+//
+//  Created by Egor Anoshin on 08.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Frontend_ScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
